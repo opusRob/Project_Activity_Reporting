8a14bb7fa303775cec1abd3a380009e7a66051cf
Rob Germain <rob.germain@opusgroupllc.com>
2015-05-15 17:28:38 -0400

    Overhauled the look and feel of the pagination.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,9 +25,39 @@ module ApplicationHelper
 		end
 	end
 	
+	def link_to_bootstrap_glyphicon_unless(condition, path, size, type, title, glyphicons, options = {}, link_html_options = {})
+		# Available options:  text_start, text_end.
+		link_to_unless condition, path, class: "btn btn-#{size} btn-#{type}", title: title, "aria-label" => title, method: options[:method], data: options[:data], html_options: link_html_options do
+			link_inner_html = options[:text_start].to_s
+			glyphicons.each do |glyphicon|
+				link_inner_html += '<span class="glyphicon glyphicon-' + glyphicon + '" aria-hidden="true"></span>'
+			end
+			link_inner_html += options[:text_end].to_s
+			
+			raw(link_inner_html)
+			
+		end
+	end
+	
 	def button_tag_bootstrap_glyphicon(button_type, name, size, type, title, glyphicons, options = {}, button_html_options = {})
 		# Available options:  text_start, text_end.
-		button_tag type: button_type, name: name, class: "btn btn-#{size} btn-#{type}", title: title, "aria-label" => title, html_options: button_html_options do
+		options[:disabled] = options.has_key?(:disabled) ? options.has_key?(:disabled) : false
+		button_tag type: button_type, name: name, class: "btn btn-#{size} btn-#{type} #{options[:class]}", disabled: options[:disabled], title: title, "aria-label" => title, onclick: options[:onclick], html_options: button_html_options do
+			button_inner_html = options[:text_start].to_s
+			glyphicons.each do |glyphicon|
+				button_inner_html += '<span class="glyphicon glyphicon-' + glyphicon + '" aria-hidden="true"></span>'
+			end
+			button_inner_html += options[:text_end].to_s
+			
+			raw(button_inner_html)
+			
+		end
+	end
+	
+	def button_to_bootstrap_glyphicon(action, button_type, name, size, type, title, glyphicons, options = {}, button_html_options = {})
+		# Available options:  text_start, text_end.
+		options[:disabled] = options.has_key?(:disabled) ? options.has_key?(:disabled) : false
+		button_to action, type: button_type, name: name, class: "btn btn-#{size} btn-#{type} #{options[:class]}", disabled: options[:disabled], title: title, "aria-label" => title, html_options: button_html_options do
 			button_inner_html = options[:text_start].to_s
 			glyphicons.each do |glyphicon|
 				button_inner_html += '<span class="glyphicon glyphicon-' + glyphicon + '" aria-hidden="true"></span>'
